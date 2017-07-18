68fd4b424180a49860724d5d8e51cf441b0b5850
Anton Oparin <mavarazy@gmail.com>
2017-07-18 14:15:56 +0200

    #1

---
+++ playground/app/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import Form from "react-jsonschema-form";
+import playground from "react-jsonschema-form-playground";
+
+let conf = {
+  schema: {
+    type: "object",
+    required: ["firstName", "lastName"],
+    properties: {
+      firstName: {
+        type: "string",
+        title: "First name",
+      },
+      lastName: {
+        type: "string",
+        title: "Last name",
+      },
+      age: {
+        type: "integer",
+        title: "Age",
+      },
+      bio: {
+        type: "string",
+        title: "Bio",
+      },
+      password: {
+        type: "string",
+        title: "Password",
+        minLength: 3,
+      },
+      telephone: {
+        type: "string",
+        title: "Telephone",
+        minLength: 10,
+      },
+    },
+  },
+  uiSchema: {
+    firstName: {
+      classNames: "success",
+      "ui:autofocus": true,
+      "ui:emptyValue": "",
+    },
+    age: {
+      "ui:widget": "updown",
+      "ui:title": "Age of person",
+    },
+    bio: {
+      "ui:widget": "textarea",
+    },
+    password: {
+      "ui:widget": "password",
+      "ui:help": "Hint: Make it strong!",
+    },
+    date: {
+      "ui:widget": "alt-datetime",
+    },
+    telephone: {
+      "ui:options": {
+        inputType: "tel",
+      },
+    },
+  },
+  formData: {
+    lastName: "Norris",
+    bio: "Roundhouse kicking asses since 1940",
+  },
+};
+
+let FormToDisplay = playground(Form);
+
+export default class ResultForm extends Component {
+  render() {
+    return <FormToDisplay {...conf} />;
+  }
+}
---
+++ webpack.config.dist.js
@@ -0,0 +1,43 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+  cache: true,
+  context: __dirname + "/src",
+  entry: {
+    main: "./index.js",
+    "engine/SimplifiedRuleEngineFactory": './engine/SimplifiedRuleEngineFactory',
+    "engine/CacheControlEngineFactory": "./engine/CacheControlEngineFactory",
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/dist/",
+    filename: "[name].js",
+    library: "JSONSchemaForm",
+    libraryTarget: "umd"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    })
+  ],
+  devtool: "source-map",
+  externals: {
+    react: {
+      root: "React",
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react"
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ["babel-loader"],
+      }
+    ]
+  }
+};
