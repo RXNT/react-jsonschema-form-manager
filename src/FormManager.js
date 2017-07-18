12c3829fbc36422faf058d00161d2954ed24c019
Anton Oparin <mavarazy@gmail.com>
2017-07-18 16:07:01 +0200

    fix #3

--- src/FormManager.js
+++ src/FormManager.js
@@ -1,5 +1,32 @@
-export class StaticFormManager {}
+class FormManager {}
 
-export class RESTFormManager {}
+const DEFAULT_KEY = "form";
+
+export class LocalStorageFormManager extends FormManager {
+  constructor(key = DEFAULT_KEY) {
+    super();
+    this.key = key;
+  }
+  submit = formData => {
+    return new Promise(resolve => {
+      localStorage.setItem(this.key, formData);
+      resolve(formData);
+    });
+  };
+  update = formData => {
+    return new Promise(resolve => {
+      localStorage.setItem(this.key, formData);
+      resolve(formData);
+    });
+  };
+}
+
+export class RESTFormManager extends FormManager {
+  constructor(url, credentials) {
+    super();
+    this.url = url;
+    this.credentials = credentials;
+  }
+}
 
 export class GraphQLFormManager {}
