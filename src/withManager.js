f9fe1f958c4be3ccd14a415934cceebd0eba7b80
Anton Oparin <mavarazy@gmail.com>
2017-09-29 15:47:06 +0300

    Small error fix

--- src/withManager.js
+++ src/withManager.js
@@ -72,7 +72,7 @@ export default function withManager(
       }
 
       componentWillUnmount() {
-        manager.stop();
+        updateStrategy.stop();
       }
 
       updateExternal = (state, callback) => {
